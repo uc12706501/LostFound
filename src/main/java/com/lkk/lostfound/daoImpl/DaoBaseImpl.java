260a6c120d14fd364d46229620f8369fea1e026d
Yuchen Deng <loaden@gmail.com>
2014-08-15 20:21:51 +0800

    实现了部分DaoBaseImpl的功能

--- src/main/java/com/lkk/lostfound/daoImpl/DaoBaseImpl.java
+++ src/main/java/com/lkk/lostfound/daoImpl/DaoBaseImpl.java
@@ -1,5 +1,40 @@
 package com.lkk.lostfound.daoImpl;
 
-public class DaoBaseImpl {
+import java.util.List;
 
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import com.lkk.lostfound.dao.DaoBase;
+
+public abstract class DaoBaseImpl<T> extends HibernateDaoSupport implements
+		DaoBase<T> {
+	protected HibernateTemplate hibernateTemplate = getHibernateTemplate();
+
+	public void save(T entity) {
+		getHibernateTemplate().save(entity);
+	}
+
+	public void delete(long entityId) {
+		hibernateTemplate.delete(get(entityId));
+	}
+
+	public T get(long entityId) {
+		return (T) hibernateTemplate.get(getEntityClass(), entityId);
+	}
+
+	public List<T> getAll() {
+		String query = "FROM " + getEntityClass().getName();
+		return (List<T>) hibernateTemplate.find(query);
+	}
+
+	public List<T> getPartial(int pageSize, int pageIndex) {
+		return null;
+	}
+
+	public List<T> Query(String queryString) {
+		return hibernateTemplate.find(queryString);
+	}
+
+	abstract Class getEntityClass();
 }
---
+++ src/main/java/com/lkk/lostfound/daoImpl/UserDaoImpl.java
@@ -0,0 +1,12 @@
+package com.lkk.lostfound.daoImpl;
+
+import java.util.List;
+
+import com.lkk.lostfound.dao.UserDao;
+import com.lkk.lostfound.model.User;
+
+public class UserDaoImpl extends DaoBaseImpl<User> implements UserDao {
+
+	
+
+}
