67c2b2e57d9ad29b7357404850137f2f770e059a
uc12706501 <476362348@qq.com>
2014-08-30 16:19:03 +0800

    修复查询时出现nullpointer的bug 1. 为Action添加dao 2. 删除hibernateTemplate字段

--- src/main/java/com/lkk/lostfound/action/admin/announcement/IndexAction.java
+++ src/main/java/com/lkk/lostfound/action/admin/announcement/IndexAction.java
@@ -1,11 +1,30 @@
 package com.lkk.lostfound.action.admin.announcement;
 
+import java.util.List;
+
+import com.lkk.lostfound.dao.AnnouncementDao;
+import com.lkk.lostfound.model.Announcement;
 import com.opensymphony.xwork2.ActionSupport;
 
 @SuppressWarnings("serial")
 public class IndexAction extends ActionSupport {
+
+	private AnnouncementDao announcementDao;
+	private List<Announcement> announcements;
+
 	@Override
 	public String execute() throws Exception {
+		announcements = announcementDao.getAll();
 		return SUCCESS;
 	}
+
+	// getter and setter
+	public void setAnnouncementDao(AnnouncementDao announcementDao) {
+		this.announcementDao = announcementDao;
+	}
+
+	public List<Announcement> getAnnouncements() {
+		return announcements;
+	}
+
 }
--- src/main/java/com/lkk/lostfound/daoImpl/DaoBaseImpl.java
+++ src/main/java/com/lkk/lostfound/daoImpl/DaoBaseImpl.java
@@ -2,21 +2,19 @@ package com.lkk.lostfound.daoImpl;
 
 import java.util.List;
 
-import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 import com.lkk.lostfound.dao.DaoBase;
 
 public abstract class DaoBaseImpl<T> extends HibernateDaoSupport implements
 		DaoBase<T> {
-	protected HibernateTemplate hibernateTemplate = getHibernateTemplate();
-
+	@SuppressWarnings("unchecked")
 	public T get(long entityId) {
-		return (T) hibernateTemplate.get(getEntityClass(), entityId);
+		return (T) getHibernateTemplate().get(getEntityClass(), entityId);
 	}
 
 	public void update(T entity) {
-		hibernateTemplate.update(entity);
+		getHibernateTemplate().update(entity);
 	}
 
 	public Long save(T entity) {
@@ -24,11 +22,12 @@ public abstract class DaoBaseImpl<T> extends HibernateDaoSupport implements
 	}
 
 	public void delete(long entityId) {
-		hibernateTemplate.delete(get(entityId));
+		getHibernateTemplate().delete(get(entityId));
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<T> getAll() {
-		return hibernateTemplate.find("find " + getEntityClassName());
+		return getHibernateTemplate().find("from " + getEntityClassName());
 	}
 
 	public List<T> getPartial(int pageSize, int pageIndex) {
