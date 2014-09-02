ab6b77196a20c22f8913942252b24bbf8208a284
uc12706501 <476362348@qq.com>
2014-09-02 09:33:50 +0800

    修改TODO，此后使用TODO进行任务管理

--- src/main/java/com/lkk/lostfound/action/admin/announcement/GetImageAction.java
+++ src/main/java/com/lkk/lostfound/action/admin/announcement/GetImageAction.java
@@ -2,13 +2,17 @@ package com.lkk.lostfound.action.admin.announcement;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import com.lkk.lostfound.model.User;
 
 public class GetImageAction extends SingleAnnouncementActionBase {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public InputStream getInputStream() {
 		if (model != null && model.getImage() != null)
 			return new ByteArrayInputStream(model.getImage());
-		// TODO: 添加默认图片的返回
+		// TODO 添加默认图片的返回
 		return null;
 	}
 }
