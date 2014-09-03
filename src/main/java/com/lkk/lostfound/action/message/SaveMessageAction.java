d629b451bf76666c00c9f88c9de09a546f0fbdd9
uc12706501 <476362348@qq.com>
2014-09-03 20:47:06 +0800

    重命名Message保存的Action

--- src/main/java/com/lkk/lostfound/action/message/SaveMessageAction.java
+++ src/main/java/com/lkk/lostfound/action/message/SaveMessageAction.java
@@ -1,6 +1,8 @@
 package com.lkk.lostfound.action.message;
 
-public class AddMessageAction extends SingleMessageActionBase {
+public class SaveMessageAction extends SingleMessageActionBase {
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	public String execute() throws Exception {
 		return SUCCESS;
